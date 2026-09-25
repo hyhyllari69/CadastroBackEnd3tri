8221468214cbed7e0a3a1ca305429b1ddcc09001
Hyllari <isadora.barbosa29@escola.pr.gov.br>
2026-09-25 08:44:42 -0300

    Add files via upload

--- Site/pages.js
+++ Site/pages.js
@@ -1,5 +1,5 @@
-function atualizarNavbar() {
-    const nome = localStorafe.getItem("nome cliente")
+function atualizar_navbar() {
+    const nome = localStorage.getItem("nome_cliente")
     const token = localStorage.getItem("token")
 
     const nav_login = document.getElementById("nav-login")
@@ -8,10 +8,17 @@ function atualizarNavbar() {
     if(token && nome) {
         nav_login.style.display = "none"
         btn_user_info.style.display = "inline"
-        btn_user_info.innerText= "Olá," + nome.split(" ")[0]
-    }   else {
+        btn_user_info.innerText= "Olá, " + nome.split(" ")[0]
+    } else {
         nav_login.style.display = "inline"
         btn_user_info.style.display = "none"
     }
 }
 
+function logout() {
+    localStorage.removeItem("nome_cliente")
+    localStorage.removeItem("token")
+    window.location.href = "login.html"
+}
+
+window.addEventListener("DOMContentLoaded", atualizar_navbar)
